52dd8aa92cb8ec95dbfacb827af57e0606abcb75
SharkMouth <haritsazfa@gmail.com>
2024-03-20 07:35:29 +0700

    menambahkan class SimpleDataBase

--- jurnal5/Program.cs
+++ jurnal5/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++) {
+            Console.WriteLine($"Data {i+1} berisi : {storedData[i]} yang disimpan pada waktu {inputDates[i]}");
+        }
+    }   
+}
+
+class main
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<int> simpleDataBase = new SimpleDataBase<int>();
+        int angka1 = 13;
+        int angka2 = 02;
+        int angka3 = 22;
+        simpleDataBase.AddNewData(angka1);
+        simpleDataBase.AddNewData(angka2);
+        simpleDataBase.AddNewData(angka3);
+        simpleDataBase.PrintAllData();
+    }
+}
